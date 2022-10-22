1beb8b6bb1b8a01d3a9c4ca2a36ac5ecea80eeb2
yemiwebby <yemiwebby@gmail.com>
2022-10-22 12:45:42 +0100

    Get todoItem ID

--- cypress/e2e/api-tests/todo.cy.js
+++ cypress/e2e/api-tests/todo.cy.js
@@ -1,10 +1,11 @@
 describe("TODO api testing", () => {
-  let todoItem = 88;
+  let todoItem;
   it("fetches Todo items - GET", () => {
     cy.request("/todos/").as("todoRequest");
     cy.get("@todoRequest").then((todos) => {
       expect(todos.status).to.eq(200);
       assert.isArray(todos.body, "Todos Response is an array");
+      todoItem = todos.body[0]._id;
     });
   });
 
